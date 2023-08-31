715b2ea443244143448a18eaa6a430262b5352b2
felipricardo <felipebarbosaok@gmail.com>
2023-08-31 10:37:20 -0300

    up

--- SistemaLogin/CadastroUsuarios.cs
+++ SistemaLogin/CadastroUsuarios.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SistemaLogin
 {
-    class CadastroUsuarios
+    static class CadastroUsuarios
     {
         private static Usuario[] usuarios =
         {
